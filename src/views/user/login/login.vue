5d3754144de3b00240fb34725ca5f191f0f8b690
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-30 08:48:18 +0530

    implement login component

---
+++ src/views/user/login/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Dunzo-Clone</h1>
+    <p @click="getLoginURL">click</p>
+    <p>{{ userLoginURL }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userLoginURL: null
+    }
+  },
+  methods: {
+    async getLoginURL() {
+      this.userLoginURL = await fetch('http://localhost:8000/user/loginURL')
+    }
+  }
+}
+</script>
+
+
+<style>
+
+</style>
