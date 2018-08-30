482795e3422a5d815598c013bfad7c9028fb88b7
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-08-30 08:48:06 +0530

    insert config file

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pages: {
+    userLogin: {
+      entry: 'src/views/user/login/login.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'login-DunzoClone'
+    }
+  }
+}
