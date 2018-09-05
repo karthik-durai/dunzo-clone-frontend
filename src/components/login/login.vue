2ca04aa8236194581c37fc82f89df843cc903ea5
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-09-05 20:05:45 +0530

    implement sign in button component and put google svg file in assets

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -2,26 +2,40 @@
   <div>
     <h1>Dunzo-Clone</h1>
     <p v-if="fetchingURL">loading</p>
-    <a v-else href="loginURL">sign in with google</a>
+    <signin-button
+      v-else
+      v-bind:loginURL="loginURL"
+    />
   </div>
 </template>
 
 <script>
+import signinButton from './signinButton.vue'
+
 export default {
+  components: {
+    signinButton
+  },
   data() {
     return {
       loginURL: null
     }
   },
   methods: {
-    getLoginURL() {
-      return 'hello, world'
+    async getLoginURL() {
+      try {
+        let fetchedObj =  await fetch('http://localhost:8000/user/getLoginURL')
+        let url = await fetchedObj.json()
+        console.log(url)
+        //  return url
+      } catch (err) {
+        return null
+      }
     }
   },
   mounted() {
     this.loginURL = this.getLoginURL()
-    console.log(this.loginURL)
-    
+    // console.log(this.loginURL)
   },
   computed: {
     fetchingURL() {
---
+++ src/components/login/signinButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="signin-button">
+    <a v-bind:href="loginURL">
+      <img src="../../assets/btn_google_light_normal_ios.svg" 
+            alt="G"
+            class="signin-button__g-image">
+      <span class ="signin-button__link-text">Sign in with Google</span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [ 'loginURL' ]
+
+}
+</script>
+
+<style scoped>
+  .signin-button {
+    border: solid;
+    display: grid;
+  }
+  .signin-button__link-text {
+    border: solid;
+    display: inline-flex;
+  }
+  .signin-button__g-image {
+    border: solid;
+  }
+</style>
