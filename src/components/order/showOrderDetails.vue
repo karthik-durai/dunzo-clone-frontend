254aca5cdddac8f7f5f4463e4e7bb47574730320
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-09-06 16:34:31 +0530

    can get order details

--- src/components/order/showOrderDetails.vue
+++ src/components/order/showOrderDetails.vue
@@ -10,10 +10,26 @@
 import vueInstance from '../../views/user/main.js'
 
 export default {
+  data() {
+    return {
+      orderDetails: null
+    }
+  },
   computed: {
     menuRoute() {
       return `/showOrders/${ vueInstance.$route.params.id }/menu`
+    },
+    getOrderDetailsUrl() {
+      return `http://localhost:8000/user/getOrderDetails?orderID=${vueInstance.$route.params.id}`
     }
+  },
+  methods: {
+    async getOrderDetails (url) {
+      return (await (await fetch(url)).json()).details
+    }
+  },
+  async mounted() {
+    this.orderDetails = await this.getOrderDetails(this.getOrderDetailsUrl)
   }
 }
 </script>
