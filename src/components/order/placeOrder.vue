3846f6ae1b8fda1cc754aea8db182c6bbaea036b
KarthikSiddarth <iamkarthiksiddarth@outlook.com>
2018-09-06 12:30:59 +0530

    implement place order functions

--- src/components/order/placeOrder.vue
+++ src/components/order/placeOrder.vue
@@ -3,21 +3,76 @@
     <router-link to='/placeOrder/menu'>menu</router-link>
     <router-view/>
     <form action="">
-      <input type="text">
-      <button>place</button>
+      <input type="text" v-model="orderDescription" v-bind:placeholder="placeholder">
+      <button @click="placeOrder">place</button>
     </form>
+    <information
+      v-bind:lessChars = "hasLessChars"
+      v-bind:failedToPlace = "hasFailedToPlace"
+      v-bind:inValidChars = "hasInvalidChars"
+      v-bind:placedSuccess = "hasPlaced"
+      />
   </div>
 </template>
 
 <script>
 import userMenu from '../menu/userMenu.vue'
+import information from '../status/placeOrderMessages/information.vue'
 
 export default {
   components: {
-    userMenu
+    userMenu,
+    information
   },
-  mounted() {
-    
+  data() {
+    return {
+      orderDescription: '',
+      placeholder: 'Type your order and click place',
+      hasLessChars: false,
+      hasFailedToPlace: false,
+      hasInvalidChars: false,
+      hasPlaced: false
+    }
+  },
+  methods: {
+    async placeOrder () {
+      try{
+     if (!this.checkIfLessChars()) {
+       this.hasLessChars = false
+       let statusObj = await fetch('http://localhost:8000/user/placeOrder', this.constructBody())
+       let status = (await statusObj.json()).status
+       determineStatusMessage(status)
+     } else {
+       this.hasLessChars = true
+     }
+      } catch (err) {
+        this.hasFailedToPlace = true
+      }
+    },
+    checkIfLessChars () {
+      return this.orderDescription.length <= 10 ? true : false
+    },
+    checkIfInvalidChars () {
+      return false
+    },
+    constructBody () {
+      return {
+        method: 'post',
+        header: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({'description': this.orderDescription})
+      }
+    },
+    determineStatusMessage (status) {
+      if (status) {
+        this.hasPlaced = true
+        this.hasFailedToPlace = false
+      } else {
+        this.hasFailedToPlace = true
+        this.hasPlaced = false
+      }
+    }
   }
 }
 </script>
---
+++ src/components/status/placeOrderMessages/information.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p v-if="lessChars">Description should be more than 10 characters</p>
+    <p v-if="inValidChars">
+      Found invalid characters in the Description, please remove or replace them</p>
+    <p v-if="placedSuccess">Your order has got place successfully</p>
+    <p v-if="failedToPlace">Sorry, your order is not placed, try again</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['lessChars', 'failedToPlace', 'inValidChars', 'placedSuccess']
+}
+</script>
+
+<style>
+
+</style>
