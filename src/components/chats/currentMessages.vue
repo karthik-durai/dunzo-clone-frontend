9176b2908f4d72d74180326c8352f5712243335f
Karthik Siddarth <iamkarthiksiddarth@outlook.com>
2018-09-18 11:38:09 +0530

    chat works in user view

---
+++ src/components/chats/currentMessages.vue
@@ -0,0 +1,17 @@
+<template>
+  <ol>
+    <li v-for="chat of chats">
+      <p>{{ chat }}</p>
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  props: ['chats']
+}
+</script>
+
+<style>
+
+</style>
--- src/components/chats/userChat.vue
+++ src/components/chats/userChat.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <past-messages v-bind:chats="pastChats" />
+    <current-messages v-bind:chats="currentChat"/>
     <input type="text" v-model="message"/>
     <button v-on:click="sendMessage">send</button>
   </div>
@@ -7,22 +9,34 @@
 
 <script>
 import vueInstance from '../../views/user/main.js'
+import pastMessages from './pastMessages.vue'
+import currentMessages from './currentMessages.vue'
 
 export default {
   props: ['socket'],
+  components: {
+    pastMessages,
+    currentMessages
+  },
   data() {
     return {
-      message: ''
+      message: '',
+      pastChats: [],
+      currentChat: []
     }
   },
   methods: {
     sendMessage () {
       this.socket.emit('chat message', [this.$route.params.id, this.message])
+      this.currentChat.push({ from: 'you', message: this.message })
+    },
+    getPastMessages (messages) {
+      this.pastChats = messages
     }
   },
   mounted() {
     this.socket.emit('join chat room', this.$route.params.id)
-    this.socket.on('past messages', console.log)
+    this.socket.on('past messages', this.getPastMessages)
   }
 }
 </script>
