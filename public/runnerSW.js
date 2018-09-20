66f43477f8bb7a278efb5cc5092b012088c2fe58
Mukul Mahajan Singraur <rammmukul@users.noreply.github.com>
2018-09-20 11:14:26 +0530

    use data.body for notification body

--- public/runnerSW.js
+++ public/runnerSW.js
@@ -1,6 +1,6 @@
 function showNotification (e) {
-    const data = e.data.json()
-    self.registration.showNotification(data.title)
+  const data = e.data.json()
+  self.registration.showNotification(data.title, {body: data.body, icon: 'http://localhost:8080/favicon.ico'})
 }
 
 self.addEventListener('push', showNotification)
